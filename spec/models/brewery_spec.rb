e5d4439d26fd29a0fe71b44eef0b8464543fea3d
Jake Volpe <volpe.jacob@gmail.com>
2021-06-14 15:58:42 -0600

    Setup brewery model/model test/db

---
+++ spec/models/brewery_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Brewery, type: :model do
+  describe 'validations' do 
+    it { should validate_uniqueness_of :db_id }
+    it { should validate_uniqueness_of :obdb_id }
+
+    it { should validate_presence_of :db_id }
+    it { should validate_presence_of :obdb_id }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :brewery_type }
+    it { should validate_presence_of :street }
+    it { should validate_presence_of :city }
+    it { should validate_presence_of :state }
+    it { should validate_presence_of :postal_code }
+  end
+end
