be995187b1efb5d537133694b94fe7e8c26ac623
Jake Volpe <volpe.jacob@gmail.com>
2021-06-16 13:24:52 -0600

    Make brewery filtered search method

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -9,4 +9,12 @@ class Brewery < ApplicationRecord
   validates :city, presence: { require: true }
   validates :state, presence: { require: true }
   validates :postal_code, presence: { require: true }
+
+  def self.filter_brewery_searches(name, postal_code, city, brewery_type)
+    Brewery.where('name ilike ?', "%#{name}%")
+    .where('postal_code ilike ?', "%#{postal_code}%")
+    .where('city ilike ?', "%#{city}%")
+    .where('brewery_type like ?', "%#{brewery_type}%")
+  end
+
 end
