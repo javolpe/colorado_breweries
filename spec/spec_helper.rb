897d66ccb6029eb4475cb0c894f407717d38efa2
Jake Volpe <volpe.jacob@gmail.com>
2021-06-17 13:06:50 -0600

    Added line to rerun circle

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -94,6 +94,7 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 
+
   def check_hash_structure(object, key, data_type)
     expect(object).to have_key(key)
     expect(object[key]).to be_a(data_type)
