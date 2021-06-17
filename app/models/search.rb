e5526e14030c508215754ac30064e2f2481b87b7
Jake Volpe <volpe.jacob@gmail.com>
2021-06-17 12:55:57 -0600

    Update models/model tests to reflect changes

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,3 +1,3 @@
 class Search < ApplicationRecord
-  validates :filter_name, uniqueness: { scope: [:filter_postal_code, :filter_city, :filter_brewery_type, :name, :postal_code, :city, :brewery_type] }
+  validates :filter_name, uniqueness: { scope: [:filter_postal_code, :filter_city, :filter_brewery_type, :sort_name, :sort_postal_code, :sort_city, :sort_brewery_type] }
 end
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -13,4 +13,40 @@ RSpec.describe Brewery, type: :model do
     it { should validate_presence_of :state }
     it { should validate_presence_of :postal_code }
   end
+  describe "Class Methods" do 
+    it "should be able to filter breweries by name" do 
+      OpenBreweryFacade.seed_db_with_colorado_breweries
+      expect(Brewery.all.count).to eq(429)
+      name = "alpine dog"
+      postal_code = nil
+      city = nil
+      brewery_type = nil
+      results = Brewery.filter_brewery_searches(name, postal_code, city, brewery_type)
+      
+      expect(results.count).to eq(1)
+      expect(results.first.name).to eq("Alpine Dog Brewing Co")
+    end
+    it "should be able to filter breweries by postal_code" do 
+      OpenBreweryFacade.seed_db_with_colorado_breweries
+      expect(Brewery.all.count).to eq(429)
+      name = nil
+      postal_code = "8021"
+      city = nil
+      brewery_type = nil
+      results = Brewery.filter_brewery_searches(name, postal_code, city, brewery_type)
+      
+      expect(results.count).to eq(38)
+    end
+    it "should not find breweries if no brewery matches" do 
+      OpenBreweryFacade.seed_db_with_colorado_breweries
+      expect(Brewery.all.count).to eq(429)
+      name = "?"
+      postal_code = nil
+      city = nil
+      brewery_type = nil
+      results = Brewery.filter_brewery_searches(name, postal_code, city, brewery_type)
+      
+      expect(results.count).to eq(0)
+    end
+  end
 end
